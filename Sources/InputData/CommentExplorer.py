9fa95a2a22f967f534becf7d095d806c404a67cb
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2025-11-21 14:24:44 +0300

    Deleting two strings

--- Sources/InputData/CommentExplorer.py
+++ Sources/InputData/CommentExplorer.py
@@ -1,20 +1,5 @@
-import re
 
 
-
-
-def youtube_id_finder():
-    url = input("\nEnter the url: ")
-
-    while True:
-        pattern = r"(?:v=|\/)([0-9A-Za-z_-]{11})"
-        match = re.search(pattern, url)
-
-        if match:
-            return match.group(1)
-        
-        else:
-            url = input("\nTry entering the url again: ")
 
 
 
