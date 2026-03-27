f64e8c948a17ad9da5197a9ad1de05d5f826d859
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2026-03-27 18:09:42 +0300

    Fixed the order of "if-elif"

--- Sources/InputData/VideoExplorer.py
+++ Sources/InputData/VideoExplorer.py
@@ -8,17 +8,17 @@ from re import fullmatch
 
 def searching_for_videos():
     region = input("What region would you like? (Enter as US, RU, UK, etc): ").upper()
-    while True:      
+    while True:
         if not region.isalpha():
             region = input("\nEnter it again in letters: ").upper()
-
-        elif len(region) != 2:
-            region = input("\nEnter again: ").upper()
 
         elif not fullmatch(r"[A-Za-z]+", region):
             region = input("\nEnter it again in English: ").upper()
 
-        elif len(region) == 2 and region.isalpha():
+        elif len(region) != 2:
+            region = input("\nEnter again: ").upper()
+
+        else:
             break
 
         
