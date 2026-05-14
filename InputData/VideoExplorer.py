bd9975ba18427d19ce342ad72a243a1585610240
Valentin Kolesnikov <valentinkolesnikov2704@gmail.com>
2026-05-15 02:20:47 +0300

    Changed error analysis in another file

--- InputData/VideoExplorer.py
+++ InputData/VideoExplorer.py
@@ -9,20 +9,12 @@ from re import fullmatch
 def searching_for_videos():
     region = input("What region would you like? (Enter as US, RU, UK, etc): ").upper()
     while True:
-        if not region.isalpha():
-            region = input("\nEnter it again in letters: ").upper()
-
-        elif not fullmatch(r"[A-Za-z]+", region):
+        if not fullmatch(r"[A-Za-z]{2}", region):
             region = input("\nEnter it again in English: ").upper()
-
-        elif len(region) != 2:
-            region = input("\nEnter again: ").upper()
-
         else:
             break
 
         
-
 
 
     keywords, ageAfter, ageBefore, duration, maximum, which_order, dimension = search_engine(playlist_enabled=False)
